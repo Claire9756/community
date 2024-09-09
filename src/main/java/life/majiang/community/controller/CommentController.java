c65e27efc6a0c4132a6ab0ab3e5fcefb75390af3
Claire9756 <155421255+Claire9756@users.noreply.github.com>
2024-09-09 11:05:37 +0800

    edit

--- src/main/java/life/majiang/community/controller/CommentController.java
+++ src/main/java/life/majiang/community/controller/CommentController.java
@@ -33,33 +33,7 @@ public class CommentController {
     @RequestMapping(value = "/comment", method = RequestMethod.POST)
     public Object post(@RequestBody CommentCreateDTO commentCreateDTO,
                        HttpServletRequest request) {
-        User user = (User) request.getSession().getAttribute("user");
-        if (user == null) {
-            return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
-        }
-
-        if (user.getDisable() != null && user.getDisable() == 1) {
-            return ResultDTO.errorOf(CustomizeErrorCode.USER_DISABLE);
-        }
-
-        if (commentCreateDTO == null || StringUtils.isBlank(commentCreateDTO.getContent())) {
-            return ResultDTO.errorOf(CustomizeErrorCode.CONTENT_IS_EMPTY);
-        }
-
-        if (questionRateLimiter.reachLimit(user.getId())) {
-            return ResultDTO.errorOf(CustomizeErrorCode.RATE_LIMIT);
-        }
-
-        Comment comment = new Comment();
-        comment.setParentId(commentCreateDTO.getParentId());
-        comment.setContent(commentCreateDTO.getContent());
-        comment.setType(commentCreateDTO.getType());
-        comment.setGmtModified(System.currentTimeMillis());
-        comment.setGmtCreate(System.currentTimeMillis());
-        comment.setCommentator(user.getId());
-        comment.setLikeCount(0L);
-        commentService.insert(comment, user);
-        return ResultDTO.okOf();
+        
     }
 
     @ResponseBody
